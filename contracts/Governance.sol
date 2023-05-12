c35f449e37c4ee5f5f91ce939f14279f10013f1a
elb-bme <79838492+elb-bme@users.noreply.github.com>
2023-05-12 18:52:46 +0200

    Ownable

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 import "./Parcel.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Governance {
+contract Governance is Ownable {
   struct SLAData {
         uint256 threshold;
         bool violated;
